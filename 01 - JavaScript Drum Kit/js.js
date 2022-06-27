594434c2a5a9ff67d60d475627b96aa25b19580f
Leonardo Loaiza <leonardodario.loaiza@unmsm.edu.pe>
2022-06-27 12:49:07 -0500

    01 - Script para reproducir audio

--- 01 - JavaScript Drum Kit/js.js
+++ 01 - JavaScript Drum Kit/js.js
@@ -1,3 +1,10 @@
 window.addEventListener('keydown', (e) => {
-    console.log(e);
+    // Seleccionar data-key correspondiente a la tecla presionada
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    // Si no hay tecla en el html return
+    if (!audio) return;
+    // Sin esto cuando presionamos teclas sucesivamente el audio seguiría hasta terminar
+    audio.currentTime = 0;
+    // Ejecutamos el audio
+    audio.play();
 });
